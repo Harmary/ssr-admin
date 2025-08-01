f6e2443a05179f847faf950597a784f450213d1b
Harmary <maria.h4rlamova@gmail.com>
2025-08-01 16:53:14 +0300

    protect aut routes

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,4 +1,13 @@
-export default function AuthLayout({ children }: { children: React.ReactNode }) {
+import { getAuthSession } from "@/lib/auth";
+import { redirect } from "next/navigation";
+
+export default async function AuthLayout({ children }: { children: React.ReactNode }) {
+    const session = await getAuthSession();
+
+    if (session) {
+        redirect('/admin/dashboard');
+    }
+
     return (
         <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-gray-100 to-gray-300 dark:from-gray-900 dark:to-gray-800">
             <div className="w-full max-w-md p-8">
